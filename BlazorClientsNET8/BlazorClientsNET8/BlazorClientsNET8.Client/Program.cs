5515e9a946cb342ef1ce37d7a41de5e119299a65
vladimiro-fs <vladzyo.gaming@pm.me>
2024-12-27 01:56:27 +0000

    Add razor components: ClientManagement and ClientsList

--- BlazorClientsNET8/BlazorClientsNET8/BlazorClientsNET8.Client/Program.cs
+++ BlazorClientsNET8/BlazorClientsNET8/BlazorClientsNET8.Client/Program.cs
@@ -1,5 +1,14 @@
+using BlazorClientsNET8.Client.Services;
+using BlazorClientsNET8.Shared.Interfaces;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped<IClientRepository, ClientService>();
+
+builder.Services.AddScoped(http => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress),
+});
 
 await builder.Build().RunAsync();
